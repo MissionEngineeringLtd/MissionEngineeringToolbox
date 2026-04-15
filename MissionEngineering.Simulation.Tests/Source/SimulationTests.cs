3fd932d093303c095ba0c7c1dfdc8fc5db5610f8
Mission Engineering <mission_engineering@outlook.com>
2026-04-15 19:46:39 +0100

    #9: Fixed test.

--- MissionEngineering.Simulation.Tests/Source/SimulationTests.cs
+++ MissionEngineering.Simulation.Tests/Source/SimulationTests.cs
@@ -20,6 +20,6 @@ public sealed class SimulationTests
         simulationHarness.Run();
 
         // Assert
-        Assert.HasCount(3, simulationHarness.Simulation.SimulationModels);
+        Assert.HasCount(4, simulationHarness.Simulation.SimulationModels);
     }
 }
