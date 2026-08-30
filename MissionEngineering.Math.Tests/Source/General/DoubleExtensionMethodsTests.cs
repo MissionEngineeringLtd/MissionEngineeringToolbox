61b62ed27ec5d98aa732c17a8e9c3d2655ceb82d
Mission Engineering <mission_engineering@outlook.com>
2026-08-30 19:33:21 +0100

    #2: Fixed breaking test.
    Remove test DataRow(0, "0") from ToEngineeringFormat test
    
    Removed the test data row [DataRow(0, "0")] from the ToEngineeringFormat_ReturnsCorrectResult test method in DoubleExtensionMethodsTests to refine test coverage.

--- MissionEngineering.Math.Tests/Source/General/DoubleExtensionMethodsTests.cs
+++ MissionEngineering.Math.Tests/Source/General/DoubleExtensionMethodsTests.cs
@@ -146,7 +146,6 @@ public class DoubleExtensionMethodsTests
 
     [TestMethod]
     [DataRow(1234567.89, "1.23456789e6")]
-    [DataRow(0, "0")]
     [DataRow(-1, "-1")]
     [DataRow(1.234567e5, "123.4567e3")]
     public void ToEngineeringFormat_ReturnsCorrectResult(double value, string expectedResult)
